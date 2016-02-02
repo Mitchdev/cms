4446d6c850e5fd2ad3b49ac1d9b2c636f2385703
Tobias Fröberg <tobias.froberg@fortnox.se>
2016-02-02 12:00:36 +0100

    img-tag is now closed as should be

--- cms.js
+++ cms.js
@@ -122,7 +122,7 @@ if (!run) {
                     '<span>',
                         '<img class="emoji" src="https://dubtrack-fm.s3.amazonaws.com/assets/emoji/images/emoji/point_right.png" title=":point_right:" alt=":point_right:" align="absmiddle"></img>',
                         '<a href="https://github.com/mitchdev/cms"> Our Github </a>',
-                        '<img class="emoji" src="https://dubtrack-fm.s3.amazonaws.com/assets/emoji/images/emoji/point_left.png" title=":point_left:" alt=":point_left:" align="absmiddle"></img>',
+                        '<img class="emoji" src="https://dubtrack-fm.s3.amazonaws.com/assets/emoji/images/emoji/point_left.png" title=":point_left:" alt=":point_left:" align="absmiddle" />',
                     '</span><br><br><br>',
                 '</li>'
             ].join('');
